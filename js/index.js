c6103fde39a1f882fac5f3b896aab2be6e257832
MariaEva91 <mariaevapizzorno@gmail.com>
2019-04-29 16:11:14 -0300

    add index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
 //**Variables */
 
+let name = '';
+let cards = [];
+
+
+//**Start game: login and choose level */
+
+$('#startButton').on('click', () => {
+    $('.menu-welcome').addClass('hide');
+    $('.menu-login').removeClass('hide');
+})
+
+$('.level').on('click', () => {
+    name = $('#input').val();
+    if (name != '') {
+        $('.menu-login').addClass('hide');
+        $('.main-board').removeClass('hide');
+    } else if (name == '') {
+        $('#err-button').removeClass('hide');
+        setTimeout(()=>{
+           $('#err-button').addClass('hide');
+        },3000)
+    }
+});
+
+//** */
+
+
